2757f8d17661d3b2d540efff6cbf0ccc681bf151
BlackDilvish <gabriel.naleznik@gmail.com>
2020-05-06 19:57:59 +0200

    Resolving another conflicts

--- covidlocalizations/settings.py
+++ covidlocalizations/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sfu$h)=-dqv6@lo8-sx4i#wr9)71&%nck5dj)de1ex!ys(@qjx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['covidlocalizations.herokuapp.com']
+ALLOWED_HOSTS = ['covidlocalizations.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
