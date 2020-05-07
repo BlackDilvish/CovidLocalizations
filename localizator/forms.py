70baac22881a99f9ddb03d572da5d654b89343a7
Excavelty <gambitek2@interia.pl>
2020-05-07 11:12:36 +0200

    =Some extended validation for json's. Changed gitignore.

--- localizator/forms.py
+++ localizator/forms.py
@@ -5,7 +5,7 @@ class FileForm(forms.Form):
     uplfile = forms.FileField(label='Select a file with localization')
 
 class StatusForm(forms.Form):
-    check = forms.BooleanField(required=False, validators=[FileExtensionValidator(allowed_extensions=['json'])])
+    check = forms.BooleanField(required=False)
     #start_date = forms.DateField(required=False)
     #end_date = forms.DateField(required=False)
 
