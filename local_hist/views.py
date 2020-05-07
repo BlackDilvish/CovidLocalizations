4caacf7c730effd12766a360bd567c898703b22c
BlackDilvish <gabriel.naleznik@gmail.com>
2020-05-07 12:56:18 +0200

    Adding some changes for localisation history

--- local_hist/views.py
+++ local_hist/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render
 from localizator.models import LocalizationsData
 
-def local_hist(response):	
-	hist = LocalizationsData.objects.filter(name = response.user.username)	
-	return render(response, 'local_hist/local_hist.html', {'output':history(hist)})
+def local_hist(response):
+	username = response.user.username	
+	hist = LocalizationsData.objects.filter(name = username)	
+	return render(response, 'local_hist/local_hist.html', {'name': username, 'output': history(hist)})
 	
 def history(hist):
 	output = []
 	for item in hist:
 		for i in range(len(item.data["timelineObjects"])):
 			if "activitySegment" in item.data["timelineObjects"][i]:
-				output.append({"startTimeOfActivity" : item.data["timelineObjects"][i]["activitySegment"]["duration"]["startTimestampMs"]})
-				output.append({"endTimeOfActivity" : item.data["timelineObjects"][i]["activitySegment"]["duration"]["endTimestampMs"]})
-				output.append({"startLatitude" : item.data["timelineObjects"][i]["activitySegment"]["startLocation"]["latitudeE7"]})
-				output.append({"startLongitude" : item.data["timelineObjects"][i]["activitySegment"]["startLocation"]["longitudeE7"]})
-				output.append({"endLatitude" : item.data["timelineObjects"][i]["activitySegment"]["endLocation"]["latitudeE7"]})
-				output.append({"endLongitude" : item.data["timelineObjects"][i]["activitySegment"]["endLocation"]["longitudeE7"]})
+				act = {}
+				act["startTimeOfActivity"] = item.data["timelineObjects"][i]["activitySegment"]["duration"]["startTimestampMs"]
+				act["endTimeOfActivity"] = item.data["timelineObjects"][i]["activitySegment"]["duration"]["endTimestampMs"]
+				act["startLatitude"] = item.data["timelineObjects"][i]["activitySegment"]["startLocation"]["latitudeE7"]
+				act["startLongitude"] = item.data["timelineObjects"][i]["activitySegment"]["startLocation"]["longitudeE7"]
+				act["endLatitude"] = item.data["timelineObjects"][i]["activitySegment"]["endLocation"]["latitudeE7"]
+				act["endLongitude"] = item.data["timelineObjects"][i]["activitySegment"]["endLocation"]["longitudeE7"]
+				output.append(act)
 			else:
-				output.append({"startTimeOfVisitingAPlace" : item.data["timelineObjects"][i]["placeVisit"]["duration"]["startTimestampMs"]})
-				output.append({"endTimeOfVisitingAPlace" : item.data["timelineObjects"][i]["placeVisit"]["duration"]["endTimestampMs"]})
-				output.append({"Place'sLatitude" : item.data["timelineObjects"][i]["placeVisit"]["location"]["latitudeE7"]})
-				output.append({"Place'sLongitude" : item.data["timelineObjects"][i]["placeVisit"]["location"]["longitudeE7"]})
+				vis = {}
+				vis["startTimeOfVisitingAPlace"] = item.data["timelineObjects"][i]["placeVisit"]["duration"]["startTimestampMs"]
+				vis["endTimeOfVisitingAPlace"] = item.data["timelineObjects"][i]["placeVisit"]["duration"]["endTimestampMs"]
+				vis["PlacesLatitude"] = item.data["timelineObjects"][i]["placeVisit"]["location"]["latitudeE7"]
+				vis["PlacesLongitude"] = item.data["timelineObjects"][i]["placeVisit"]["location"]["longitudeE7"]
+				output.append(vis)
 	return output
 	
