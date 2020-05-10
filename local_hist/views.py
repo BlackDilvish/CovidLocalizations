af966e2b65c7ecf9dac670da47fc6e6997d580c9
BlackDilvish <gabriel.naleznik@gmail.com>
2020-05-10 22:32:43 +0200

    Color update and splitting history function

--- local_hist/views.py
+++ local_hist/views.py
@@ -24,60 +24,9 @@ def history(hist):
 	for item in hist:
 		for i in range(len(item.data["timelineObjects"])):
 			if "activitySegment" in item.data["timelineObjects"][i]:
-				act = {}
-				
-				if "duration" in item.data["timelineObjects"][i]["activitySegment"] and "startTimestampMs" in item.data["timelineObjects"][i]["activitySegment"]["duration"]:
-					act["Start_time"] = item.data["timelineObjects"][i]["activitySegment"]["duration"]["startTimestampMs"]
-				
-				if "duration" in item.data["timelineObjects"][i]["activitySegment"] and "endTimestampMs" in item.data["timelineObjects"][i]["activitySegment"]["duration"]:  
-					act["End_time"] = item.data["timelineObjects"][i]["activitySegment"]["duration"]["endTimestampMs"]
-				 
-				if "startLocation" in item.data["timelineObjects"][i]["activitySegment"] and "latitudeE7" in item.data["timelineObjects"][i]["activitySegment"]["startLocation"]: 
-					act["Start_latitude"] = item.data["timelineObjects"][i]["activitySegment"]["startLocation"]["latitudeE7"]
-				
-				if "startLocation" in item.data["timelineObjects"][i]["activitySegment"] and "longitudeE7" in item.data["timelineObjects"][i]["activitySegment"]["startLocation"]: 
-					act["Start_longitude"] = item.data["timelineObjects"][i]["activitySegment"]["startLocation"]["longitudeE7"]
-				
-				if "endLocation" in item.data["timelineObjects"][i]["activitySegment"] and "latitudeE7" in item.data["timelineObjects"][i]["activitySegment"]["endLocation"]: 
-					act["End_latitude"] = item.data["timelineObjects"][i]["activitySegment"]["endLocation"]["latitudeE7"]
-				
-				if "endLocation" in item.data["timelineObjects"][i]["activitySegment"] and "longitudeE7" in item.data["timelineObjects"][i]["activitySegment"]["endLocation"]: 
-					act["End_longitude"] = item.data["timelineObjects"][i]["activitySegment"]["endLocation"]["longitudeE7"]
-				
-				if "distance" in item.data["timelineObjects"][i]["activitySegment"]:
-					act["Distance"] = str(item.data["timelineObjects"][i]["activitySegment"]["distance"]) #string zeby nie konwertowalo na wspolrzedne
-				if "activityType" in item.data["timelineObjects"][i]["activitySegment"]:
-					act["Activity_type"] = item.data["timelineObjects"][i]["activitySegment"]["activityType"]
-				
-				if "activities" in item.data["timelineObjects"][i]["activitySegment"] and "probability" in item.data["timelineObjects"][i]["activitySegment"]["activities"][0]:
-					act["Probability"] = item.data["timelineObjects"][i]["activitySegment"]["activities"][0]["probability"]
-				
-				output.append(act)
+				output.append(item_activity(item, i))
 			else:
-				vis = {}
-				
-				if "duration" in item.data["timelineObjects"][i]["placeVisit"] and "startTimestampMs" in item.data["timelineObjects"][i]["placeVisit"]["duration"]:
-					vis["Start_time"] = item.data["timelineObjects"][i]["placeVisit"]["duration"]["startTimestampMs"]
-				
-				if "duration" in item.data["timelineObjects"][i]["placeVisit"] and "endTimestampMs" in item.data["timelineObjects"][i]["placeVisit"]["duration"]:
-					vis["End_time"] = item.data["timelineObjects"][i]["placeVisit"]["duration"]["endTimestampMs"]
-				
-				if "location" in item.data["timelineObjects"][i]["placeVisit"] and "latitudeE7" in item.data["timelineObjects"][i]["placeVisit"]["location"]:
-					vis["Places_latitude"] = item.data["timelineObjects"][i]["placeVisit"]["location"]["latitudeE7"]
-				
-				if "location" in item.data["timelineObjects"][i]["placeVisit"] and "longitudeE7" in item.data["timelineObjects"][i]["placeVisit"]["location"]:
-					vis["Places_longitude"] = item.data["timelineObjects"][i]["placeVisit"]["location"]["longitudeE7"]
-				
-				if "location" in item.data["timelineObjects"][i]["placeVisit"] and "address" in item.data["timelineObjects"][i]["placeVisit"]["location"]:
-					vis["Address"] = item.data["timelineObjects"][i]["placeVisit"]["location"]["address"]
-				
-				if "location" in item.data["timelineObjects"][i]["placeVisit"] and "locationConfidence" in item.data["timelineObjects"][i]["placeVisit"]["location"]:
-					vis["Location_confidence"] = item.data["timelineObjects"][i]["placeVisit"]["location"]["locationConfidence"]
-				
-				if "visitConfidence" in item.data["timelineObjects"][i]["placeVisit"]:
-					vis["Visit_confidence"] = float(item.data["timelineObjects"][i]["placeVisit"]["visitConfidence"]) 
-				
-				output.append(vis)
+				output.append(item_visit(item, i))
 	return output
 	
 def convert(output):
@@ -89,4 +38,58 @@ def convert(output):
 				item[key] = str(float(val)/10e6) #na stringa zeby mapa dzialala
 	return output
 		
+def item_activity(item, i):
+	act = {}			
+	if "duration" in item.data["timelineObjects"][i]["activitySegment"] and "startTimestampMs" in item.data["timelineObjects"][i]["activitySegment"]["duration"]:
+		act["Start_time"] = item.data["timelineObjects"][i]["activitySegment"]["duration"]["startTimestampMs"]
+
+	if "duration" in item.data["timelineObjects"][i]["activitySegment"] and "endTimestampMs" in item.data["timelineObjects"][i]["activitySegment"]["duration"]:  
+		act["End_time"] = item.data["timelineObjects"][i]["activitySegment"]["duration"]["endTimestampMs"]
+		
+	if "startLocation" in item.data["timelineObjects"][i]["activitySegment"] and "latitudeE7" in item.data["timelineObjects"][i]["activitySegment"]["startLocation"]: 
+		act["Start_latitude"] = item.data["timelineObjects"][i]["activitySegment"]["startLocation"]["latitudeE7"]
 	
+	if "startLocation" in item.data["timelineObjects"][i]["activitySegment"] and "longitudeE7" in item.data["timelineObjects"][i]["activitySegment"]["startLocation"]: 
+		act["Start_longitude"] = item.data["timelineObjects"][i]["activitySegment"]["startLocation"]["longitudeE7"]
+	
+	if "endLocation" in item.data["timelineObjects"][i]["activitySegment"] and "latitudeE7" in item.data["timelineObjects"][i]["activitySegment"]["endLocation"]: 
+		act["End_latitude"] = item.data["timelineObjects"][i]["activitySegment"]["endLocation"]["latitudeE7"]
+	
+	if "endLocation" in item.data["timelineObjects"][i]["activitySegment"] and "longitudeE7" in item.data["timelineObjects"][i]["activitySegment"]["endLocation"]: 
+		act["End_longitude"] = item.data["timelineObjects"][i]["activitySegment"]["endLocation"]["longitudeE7"]
+	
+	if "distance" in item.data["timelineObjects"][i]["activitySegment"]:
+		act["Distance"] = str(item.data["timelineObjects"][i]["activitySegment"]["distance"]) #string zeby nie konwertowalo na wspolrzedne
+	if "activityType" in item.data["timelineObjects"][i]["activitySegment"]:
+		act["Activity_type"] = item.data["timelineObjects"][i]["activitySegment"]["activityType"]
+	
+	if "activities" in item.data["timelineObjects"][i]["activitySegment"] and "probability" in item.data["timelineObjects"][i]["activitySegment"]["activities"][0]:
+		act["Probability"] = item.data["timelineObjects"][i]["activitySegment"]["activities"][0]["probability"]
+
+	return act
+
+def item_visit(item, i):
+	vis = {}
+				
+	if "duration" in item.data["timelineObjects"][i]["placeVisit"] and "startTimestampMs" in item.data["timelineObjects"][i]["placeVisit"]["duration"]:
+		vis["Start_time"] = item.data["timelineObjects"][i]["placeVisit"]["duration"]["startTimestampMs"]
+	
+	if "duration" in item.data["timelineObjects"][i]["placeVisit"] and "endTimestampMs" in item.data["timelineObjects"][i]["placeVisit"]["duration"]:
+		vis["End_time"] = item.data["timelineObjects"][i]["placeVisit"]["duration"]["endTimestampMs"]
+	
+	if "location" in item.data["timelineObjects"][i]["placeVisit"] and "latitudeE7" in item.data["timelineObjects"][i]["placeVisit"]["location"]:
+		vis["Places_latitude"] = item.data["timelineObjects"][i]["placeVisit"]["location"]["latitudeE7"]
+	
+	if "location" in item.data["timelineObjects"][i]["placeVisit"] and "longitudeE7" in item.data["timelineObjects"][i]["placeVisit"]["location"]:
+		vis["Places_longitude"] = item.data["timelineObjects"][i]["placeVisit"]["location"]["longitudeE7"]
+	
+	if "location" in item.data["timelineObjects"][i]["placeVisit"] and "address" in item.data["timelineObjects"][i]["placeVisit"]["location"]:
+		vis["Address"] = item.data["timelineObjects"][i]["placeVisit"]["location"]["address"]
+	
+	if "location" in item.data["timelineObjects"][i]["placeVisit"] and "locationConfidence" in item.data["timelineObjects"][i]["placeVisit"]["location"]:
+		vis["Location_confidence"] = item.data["timelineObjects"][i]["placeVisit"]["location"]["locationConfidence"]
+	
+	if "visitConfidence" in item.data["timelineObjects"][i]["placeVisit"]:
+		vis["Visit_confidence"] = float(item.data["timelineObjects"][i]["placeVisit"]["visitConfidence"]) 
+
+	return vis
