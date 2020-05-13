97dfd0da18df96a19d3068b19b37ad612a5ab596
Grzybiarz47 <j.zajda47@gmail.com>
2020-05-13 11:18:55 +0200

    List meetings update

--- list_meetings/urls.py
+++ list_meetings/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_meetings, name="local_hist"),
+    path('', views.list_meetings, name="list_meetings"),
 ]
