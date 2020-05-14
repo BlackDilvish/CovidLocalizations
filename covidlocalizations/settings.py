1d62900fedd3c590ff52a2a152423f548678d66d
BlackDilvish <gabriel.naleznik@gmail.com>
2020-05-15 00:19:19 +0200

    Whitenoises 5

--- covidlocalizations/settings.py
+++ covidlocalizations/settings.py
@@ -136,7 +136,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, 'static'),
-	os.path.join(BASE_DIR, 'heatmap\static\heatmap'),
 )
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
