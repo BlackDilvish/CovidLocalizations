dd1a355415e57b261d10ac043145709a14a0a753
BlackDilvish <gabriel.naleznik@gmail.com>
2020-05-15 00:05:16 +0200

    Whitenoise test

--- covidlocalizations/settings.py
+++ covidlocalizations/settings.py
@@ -129,14 +129,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+    # os.path.join(BASE_DIR, 'static'),
+# )
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
