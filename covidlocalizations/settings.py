6bb149d8c9be60b4c1809082c86881cf8c900a07
BlackDilvish <gabriel.naleznik@gmail.com>
2020-05-15 00:14:51 +0200

    Whitenoise test 5

--- covidlocalizations/settings.py
+++ covidlocalizations/settings.py
@@ -134,10 +134,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
- STATICFILES_DIRS = (
-     os.path.join(BASE_DIR, 'static'),
-     os.path.join(BASE_DIR, 'heatmap\static\heatmap'),
- )
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static'),
+	os.path.join(BASE_DIR, 'heatmap\static\heatmap'),
+)
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
