618495f8ce623fde3a4c1d21e40d3f81f764463a
BlackDilvish <gabriel.naleznik@gmail.com>
2020-05-15 08:47:28 +0200

    Removing static from root dir

--- covidlocalizations/settings.py
+++ covidlocalizations/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-	os.path.join(BASE_DIR, 'static'),
+	os.path.join(BASE_DIR, 'covidlocalizations/static'),
 )
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
