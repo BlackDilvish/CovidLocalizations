c58fffdbfe06dfdb8c1d456074587e9429f4be60
BlackDilvish <gabriel.naleznik@gmail.com>
2020-06-02 23:03:54 +0200

    Some register app tests

--- register/tests.py
+++ register/tests.py
@@ -1,9 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 class LoginTestCase(TestCase):
     def setUp(self):
-        self.x = 2
+        self.client = Client()
 
-    def test_2_eq_2(self):
-        self.assertEqual(self.x, 2)
+    def test_register_get(self):
+        response = self.client.get('/register/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_empty_post(self):
+        response = self.client.post(path='/register/', data={'username':'', 'password':''})
+
+        self.assertIn(b'This field is required.', response.content)
+        self.assertEqual(response.status_code, 200)
+
+    def test_loggedout(self):
+        response = self.client.post(path='/register/loggedout') 
+
+        self.assertIn(b'You\'ve been logged out', response.content)
+        self.assertEqual(response.status_code, 200)
 
