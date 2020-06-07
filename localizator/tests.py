bee307fdb2a5f8fbfb0b3451ae59a7268a7a2666
BlackDilvish <gabriel.naleznik@gmail.com>
2020-06-07 12:58:45 +0200

    Tests for localizator models

--- localizator/tests.py
+++ localizator/tests.py
@@ -1,9 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from datetime import date
 from . import views
+from . import models
 
-class LocalizatorTestCase(TestCase):
+class LocalizatorTestViews(TestCase):
     def setUp(self):
-        pass
+        self.client = Client()
 
     def test_get_mail_address(self):
         self.assertEqual(views.get_mail_address(), 
@@ -23,3 +25,36 @@ class LocalizatorTestCase(TestCase):
 
     def test_check_for_label(self):
         self.assertEqual(views.check_for_label('nolabel'), False)
+
+    def test_index(self):
+        response = self.client.get('/home')
+        self.assertEqual(response.status_code, 200)
+
+
+class LocalizatorTestModels(TestCase):
+    def setUp(self):
+        pass
+
+    def test_localizations_data(self):
+        loc_data = models.LocalizationsData()
+
+        loc_data.name = 'name'
+        loc_data.file_date = '01.01.2020'
+
+        self.assertEqual(str(loc_data), 'name')
+        self.assertEqual(loc_data.date(), str(date.today()))
+        self.assertEqual(loc_data.json_file_date(), '01.01.2020')
+
+    def test_health_status(self):
+        health = models.HealthStatus()
+
+        health.name = 'name'
+        health.status = True
+        health.start_date = date.today()
+        health.end_date = date.today()
+
+        self.assertEqual(str(health), 'name')
+        self.assertEqual(health.covid_status(), True)
+        self.assertEqual(health.covid_start_date(), str(date.today()))
+        self.assertEqual(health.covid_end_date(), str(date.today()))
+
