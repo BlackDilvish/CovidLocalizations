8e6bd9e145ae80ac0cdf8df59ecbee1acafdcc12
olgaaaglo <61839440+olgaaaglo@users.noreply.github.com>
2020-06-18 13:18:13 +0200

    Update views.py

--- local_hist/views.py
+++ local_hist/views.py
@@ -68,8 +68,10 @@ def convert(output):
 			elif isinstance(val, int):
 				item[key] = round(float(val / divider), precision)
 		if "Waypoints" in item:
-		    if "Start_latitude" in item and "Start_longitude" in item and "End_latitude" in item and "End_longitude" in item:
-			    item["Waypoints"] = prepare_waypoints(item["Waypoints"], [item["Start_latitude"], item["Start_longitude"]], 
+		    if "Start_latitude" in item and "Start_longitude" in item and \ 
+		       "End_latitude" in item and "End_longitude" in item:
+			    item["Waypoints"] = prepare_waypoints(item["Waypoints"], 
+								  [item["Start_latitude"], item["Start_longitude"]], 
 								  [item["End_latitude"], item["End_longitude"]])
 	return output
 
